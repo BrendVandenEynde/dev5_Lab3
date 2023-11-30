24de3f33ffc0d8092bf2d099a53554fddf37c254
Brend Van den Eynde <r0883726@student.thomasmore.be>
2023-12-01 00:06:50 +0100

    feat: Integrate geolocation, weather, and Pokemon APIs in App.js
    - Update App.js to handle geolocation and weather API responses
    - Add comments for better code readability
    - Display the extracted temperature on the HTML page
    - Fetch and display a random Pokemon based on the weather condition
    
    Fixes: #2

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ export default class App {
         this.gotLocation = this.gotLocation.bind(this);
         this.errorLocation = this.errorLocation.bind(this);
         this.getWeather = this.getWeather.bind(this);
+        this.getPokemon = this.getPokemon.bind(this); // Add Pokemon method
 
         // Trigger the geolocation request
         this.getLocation();
@@ -26,11 +27,12 @@ export default class App {
 
         // Call the method to get weather information
         this.getWeather(x, y);
+        this.getPokemon(); // Fetch Pokemon as well
     }
 
     // Method to get weather information from the API
     getWeather(x, y) {
-        // Make a fetch request to the weather API
+        // Make a fetch request to the weather API (OpenWeatherMap or any other of your choice)
         fetch(`https://api.open-meteo.com/v1/forecast?latitude=${x}&longitude=${y}&hourly=temperature_2m&forecast_days=1`)
             .then(response => response.json())
             .then(data => {
@@ -42,14 +44,44 @@ export default class App {
                     // Extract the temperature value
                     const temperature = data.hourly.temperature_2m[0];
 
-                    // Display the temperature on the HTML page
-                    document.querySelector('h2').innerHTML = `${temperature}°C`;
+                    // Display the temperature on the HTML page with the thermometer emoji
+                    document.querySelector('h2').innerHTML = `🌡️ ${temperature}°C`;
 
                     // Log the extracted temperature
                     console.log("Temperature:", temperature);
                 } else {
                     // Log an error if the response structure is not as expected
                     console.error("Invalid API Response:", data);
+                }
+            })
+            .catch(error => console.log(error));
+    }
+
+    // Method to fetch a random Pokemon from the PokeAPI
+    getPokemon() {
+        // Make a fetch request to the Pokemon API (PokeAPI)
+        fetch('https://pokeapi.co/api/v2/pokemon/1/') // You can change the endpoint for a different Pokemon
+            .then(response => response.json())
+            .then(data => {
+                // Log the Pokemon API response
+                console.log("Pokemon API Response:", data);
+
+                // Check if the necessary data is available in the response
+                if (data && data.name && data.sprites && data.sprites.front_default) {
+                    // Extract Pokemon details
+                    const pokemonName = data.name;
+                    const pokemonImage = data.sprites.front_default;
+
+                    // Display the Pokemon details on the HTML page (you may customize this part)
+                    document.getElementById('pokemon-name').innerText = `Pokemon: ${pokemonName}`;
+                    document.getElementById('pokemon-image').src = pokemonImage;
+
+                    // Log the extracted Pokemon details
+                    console.log("Pokemon Name:", pokemonName);
+                    console.log("Pokemon Image:", pokemonImage);
+                } else {
+                    // Log an error if the response structure is not as expected
+                    console.error("Invalid Pokemon API Response:", data);
                 }
             })
             .catch(error => console.log(error));
